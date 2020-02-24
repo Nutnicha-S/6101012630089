55784a4efbc89e27286cc4e75b0eca5f9f6c02d6
Nutnicha Sriwallaphanon <s6101012630089@email.kmutnb.ac.th>
2020-02-24 15:32:45 +0700

    Try to create template but template doesn't exist

--- superlists/urls.py
+++ superlists/urls.py
@@ -13,9 +13,13 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls import url
+from lists import views
+
+from django.urls import path, include
 from django.contrib import admin
-from django.urls import path
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    url(r'home', views.calculator, name='home'),
+    #path('admin/', admin.site.urls),
 ]
